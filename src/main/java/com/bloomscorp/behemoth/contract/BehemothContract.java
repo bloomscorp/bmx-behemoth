58d0c7ba72843feb172b8e85453337eb9e368e04
Debabrata Acharya <11374405+onclave@users.noreply.github.com>
2023-04-17 23:51:04 +0530

    add BehemothORM

---
+++ src/main/java/com/bloomscorp/behemoth/contract/BehemothContract.java
@@ -0,0 +1,9 @@
+package com.bloomscorp.behemoth.contract;
+
+public final class BehemothContract {
+
+    private BehemothContract() {}
+
+    public static final String ID = "id";
+    public static final String VERSION = "version";
+}
---
+++ src/main/java/com/bloomscorp/behemoth/orm/BehemothORM.java
@@ -0,0 +1,37 @@
+package com.bloomscorp.behemoth.orm;
+
+import com.bloomscorp.behemoth.contract.BehemothContract;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Setter;
+
+@MappedSuperclass
+public class BehemothORM {
+
+    @Id
+    @Column(
+            name = BehemothContract.ID,
+            columnDefinition = "BIGSERIAL",
+            nullable = false
+    )
+    @SequenceGenerator(
+            name = "behemoth_id_sequence",
+            sequenceName = "behemoth_id_sequence",
+            initialValue = 1,
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "behemoth_id_sequence"
+    )
+    public Long id;
+
+    @Version
+    @Setter(AccessLevel.PROTECTED)
+    @Column(
+            name = BehemothContract.VERSION,
+            columnDefinition = "BIGSERIAL",
+            nullable = false
+    )
+    public Long version;
+}
